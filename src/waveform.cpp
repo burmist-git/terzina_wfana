5e8290743b34258db3af935d6e2a5d7a5df61de0
burmist <burm.leonid@gmail.com>
2022-10-02 08:48:24 +0200

    add analisis

--- src/waveform.cpp
+++ src/waveform.cpp
@@ -24,7 +24,9 @@
 using namespace std;
 
 waveform::waveform(){
-  _nPoints = 0;
+  _nPoints = 1;
+  _wfA = new double[_nPoints];
+  _wfT = new double[_nPoints];
 }
 
 waveform::waveform(TGraph *gr){
@@ -43,4 +45,43 @@ waveform::waveform(TGraph *gr){
 waveform::~waveform(){  
   delete _wfT;
   delete _wfA;
+}
+
+void waveform::GetMaximumTimeAmpl_FWHM_FWTM( Double_t &wf_tm, Double_t &wf_am, Double_t &wf_sig_fwhm, Double_t &wf_sig_fwtm){
+  wf_tm = -999.0;
+  wf_am = -999.0;
+  wf_sig_fwhm = -999.0;
+  wf_sig_fwtm = -999.0;
+  Int_t i_a_max = 0;
+  for(Int_t i = 0;i<_nPoints;i++){
+    if(wf_am < _wfA[i]){
+      wf_am = _wfA[i];
+      i_a_max = i;
+    }
+  }
+  if(i_a_max>=0 && i_a_max<_nPoints){
+    wf_tm = _wfT[i_a_max];
+  }
+  else
+    assert(0);
+  wf_sig_fwhm = findSigWidth(i_a_max,wf_am/2.0);
+  wf_sig_fwtm = findSigWidth(i_a_max,wf_am/10.0);
+}
+
+Double_t waveform::findSigWidth( Int_t i0, Double_t th){
+  Double_t wf_sig_t_L = 0.0;
+  Double_t wf_sig_t_R = 0.0;
+  for(Int_t i = i0;i>0;i--){
+    if(_wfA[i]>th && _wfA[i-1]<=th){
+      wf_sig_t_L = _wfT[i];
+      break;
+    }
+  }
+  for(Int_t i = i0;i<(_nPoints-1);i++){
+    if(_wfA[i]>th && _wfA[i+1]<=th){
+      wf_sig_t_R = _wfT[i];
+      break;
+    }
+  }
+  return (wf_sig_t_R - wf_sig_t_L);
 }
--- src/waveform.hh
+++ src/waveform.hh
@@ -13,6 +13,8 @@ public :
   waveform(TGraph *gr);
   ~waveform();
   inline Int_t GetnPoints(){return _nPoints;}
+  void GetMaximumTimeAmpl_FWHM_FWTM( Double_t &wf_tm, Double_t &wf_am, Double_t &wf_sig_fwhm, Double_t &wf_sig_fwtm);
+  Double_t findSigWidth( Int_t i0, Double_t th);
   
 private : 
   Int_t _nPoints;
